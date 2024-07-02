9bf4eddfd97bc3acf96db5dfcb4181817106398a
TripleCrownDiamond <gagbahungba2019@gmail.com>
2024-07-02 22:39:26 +0200

    add-layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,28 @@
 import React from 'react';
+import { Inter, Space_Grotesk } from 'next/font/google';
+import './globals.css';
+import type { Metadata } from 'next';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  variable: '--font-spaceGrotesk',
+  weight: ['300', '400', '500', '600', '700']
+});
+
+export const metadata: Metadata = {
+  title: 'DevCommunity',
+  description: 'DevCommunity is a community for developers.',
+  icons: {
+    icon: '/assets/images/site-logo.png'
+  }
+}
+
 import {
   ClerkProvider,
   SignInButton,
@@ -6,22 +30,24 @@ import {
   SignedOut,
   UserButton
 } from '@clerk/nextjs';
-import './globals.css';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary_500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          <h1 className='h1-bold'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
           {children}
         </body>
       </html>
